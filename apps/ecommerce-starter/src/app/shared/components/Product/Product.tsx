4ff59a9b1aa22fc228793cf776479a6831905bdb
yusuf_sargin <yusuf.sargin@sebit.com.tr>
2020-12-21 21:23:06 +0300

    product updated

--- apps/ecommerce-starter/src/app/shared/components/Product/Product.tsx
+++ apps/ecommerce-starter/src/app/shared/components/Product/Product.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { IProduct } from '../../interfaces/product/product.interface';
 
-export const Product = ({ _id, name, description }: IProduct) => {
+export const Product = ({ _id, name, description, image }: IProduct) => {
    return (
       <div>
          <h3>{name}</h3>
          <p>{description}</p>
+         <img src={image} alt="test" />
       </div>
    );
 };
