49e33c1f2edd4cd6ee455538303d6301fde0919e
yusuf_sargin <yusuf.sargin@sebit.com.tr>
2020-12-21 21:21:21 +0300

    product component implemented.

--- apps/ecommerce-starter/src/app/Views/Home/HomeScreen.tsx
+++ apps/ecommerce-starter/src/app/Views/Home/HomeScreen.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import products from '../../../assets/dummyData/products';
+import { Product } from '../../shared/components/Product';
+import { IProduct } from '../../shared/interfaces/product/product.interface';
 
 export const HomeScreen = () => {
    return (
       <>
          <h1>Latests Products</h1>
          <Row>
-            {products.map((item, index) => {
+            {products.map((item: IProduct, index) => {
                return (
                   <Col sm={12} md={6} lg={4} xl={3} key={index}>
-                     <h3>{item.name}</h3>
+                     <Product {...item} />
                   </Col>
                );
             })}
--- apps/ecommerce-starter/src/app/shared/components/Product/Product.tsx
+++ apps/ecommerce-starter/src/app/shared/components/Product/Product.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { IProduct } from '../../interfaces/product/product.interface';
 
-export const Product = () => {
-   return <div></div>;
+export const Product = ({ _id, name, description }: IProduct) => {
+   return (
+      <div>
+         <h3>{name}</h3>
+         <p>{description}</p>
+      </div>
+   );
 };
